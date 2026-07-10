83acb1bba73e0ee38a0ab47153c176068e6ae8d4
Intern25timewise <ujjwal@timewise.in>
2026-07-10 15:08:41 +0530

    final commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  fs: "empty",
   // ✅ Do NOT add experimental.turbo or config.turbopack anymore
   // ✅ Turbopack is automatically enabled when running `next dev --turbopack`
 };
